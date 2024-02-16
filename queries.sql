137d56cdcccf7682900e95ebeef48bef9a1b1815
Jaishali <142702371+Jaishali@users.noreply.github.com>
2024-02-15 19:30:28 -0600

    updated

--- queries.sql
+++ queries.sql
@@ -7,5 +7,5 @@ DROP TABLE job;
 --Part 4
 SELECT * FROM skill
 LEFT JOIN job_skills ON skill.id = job_skills.skills_id
-WHERE job_skills.job_id IS NOT NULL
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
